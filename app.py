ab913aa3f65f294f4f70222b77ab0571721692ca
Abhay Sharma <115949147+coderabhaysharma@users.noreply.github.com>
2025-03-31 23:34:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def predict():
         return render_template('index.html', result=f"⚠️ Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
